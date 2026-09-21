b8e07a60f4f556ce4cf2d5ae7f48613f991a86da
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2026-09-21 22:22:33 +0200

    Apply reviewer's suggestion

--- src/pydicom/uid.py
+++ src/pydicom/uid.py
@@ -95,14 +95,7 @@ class UID(str):
         if self.is_transfer_syntax:
             if not self.is_private:
                 # Implicit VR Little Endian
-                if self == "1.2.840.10008.1.2":  # noqa: SIM103
-                    return True
-
-                # Explicit VR Little Endian
-                # Explicit VR Big Endian
-                # Deflated Explicit VR Little Endian
-                # All encapsulated transfer syntaxes
-                return False
+                return self == "1.2.840.10008.1.2"
 
             return self._PRIVATE_TS_ENCODING[0]
 
@@ -114,14 +107,7 @@ class UID(str):
         if self.is_transfer_syntax:
             if not self.is_private:
                 # Explicit VR Big Endian
-                if self == "1.2.840.10008.1.2.2":  # noqa: SIM103
-                    return False
-
-                # Explicit VR Little Endian
-                # Implicit VR Little Endian
-                # Deflated Explicit VR Little Endian
-                # All encapsulated transfer syntaxes
-                return True
+                return self != "1.2.840.10008.1.2.2"
 
             return self._PRIVATE_TS_ENCODING[1]
 
@@ -140,14 +126,7 @@ class UID(str):
         """Return ``True`` if a deflated transfer syntax UID."""
         if self.is_transfer_syntax:
             # Deflated Explicit VR Little Endian
-            if self == "1.2.840.10008.1.2.1.99":  # noqa: SIM103
-                return True
-
-            # Explicit VR Little Endian
-            # Implicit VR Little Endian
-            # Explicit VR Big Endian
-            # All encapsulated transfer syntaxes
-            return False
+            return self == "1.2.840.10008.1.2.1.99"
 
         raise ValueError("UID is not a transfer syntax.")
 
