6ed593149b9456f4f936caf99ed8edbc64ab2678
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2026-09-21 22:06:13 +0200

    Revert "Apply ruff/flake8-pyi rule PYI019"
    This reverts commit e49f599dc3a13178df3e74a401c42112cf9c0a36.

--- src/pydicom/filebase.py
+++ src/pydicom/filebase.py
@@ -5,12 +5,7 @@ from io import BytesIO
 import os
 from struct import Struct
 from types import TracebackType
-from typing import TYPE_CHECKING, cast, Any, Protocol
-
-try:
-    from typing import Self  # type: ignore[attr-defined]
-except ImportError:
-    from typing_extensions import Self  # Python <= 3.10
+from typing import TYPE_CHECKING, cast, Any, TypeVar, Protocol
 
 if TYPE_CHECKING:  # pragma: no cover
     from collections.abc import Callable
@@ -19,6 +14,7 @@ if TYPE_CHECKING:  # pragma: no cover
 ExitException = tuple[
     type[BaseException] | None, BaseException | None, TracebackType | None
 ]
+Self = TypeVar("Self", bound="DicomIO")
 
 
 class ReadableBuffer(Protocol):
